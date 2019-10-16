1e68eb0b7ccf52e927d579618dd2ab47f74d2f1b
Julien TASSIN <julien.tassin.pro@gmail.com>
2019-10-17 01:21:54 +0200

    feat: v2 full SSE

--- src/__tests__/index.test.ts
+++ src/__tests__/index.test.ts
@@ -1,66 +1,143 @@
-import { MongoShell, ERROR, SUCCESS } from '../index';
+import { MongoShell, Logger } from '../index';
+import * as pWaitFor from 'p-wait-for';
+
+const logger: Logger = {
+  debug: jest.fn(),
+  info: jest.fn(),
+  warn: jest.fn(),
+  error: jest.fn(),
+};
 
 function randomDB() {
-  return Math.random()
-    .toString(36)
-    .substring(7);
+  return (
+    'dd' +
+    Math.random()
+      .toString(36)
+      .substring(7)
+  );
 }
 
-describe('MongoShell', () => {
-  describe('sendCommand', () => {
-    let mongoShell: MongoShell;
+describe('Shell', () => {
+  let shell: MongoShell;
 
-    afterEach(async () => {
-      if (mongoShell) {
-        mongoShell.destroy();
+  describe('destroy', () => {
+    it('kill the subprocess', async () => {
+      shell = new MongoShell('42', 'localhost:27017', logger);
+      await shell.init();
+      await shell.destroy();
+    });
+  });
+  describe('sendCommand', () => {
+    afterEach(() => {
+      if (shell) {
+        shell.destroy();
       }
     });
 
-    it('can sends a big mongo command', async () => {
+    it('can sends a command to a mongodb', async () => {
+      shell = new MongoShell('42', 'localhost:27017', logger);
+      await shell.init();
+      const outs = new Array<string>();
+      shell.stdout.on('data', chunk => {
+        outs.push(chunk.toString());
+      });
+      shell.sendCommand({ in: 'foo=5' });
+      await pWaitFor(() => {
+        return outs.length === 1;
+      });
+      const last = Array.from(outs).pop();
+      expect(last).toEqual('5\n');
+    });
+
+    it('keeps env from a call to another', async () => {
+      shell = new MongoShell('42', 'localhost:27017', logger);
+      await shell.init();
+      const outs = new Array<string>();
+      shell.stdout.on('data', chunk => {
+        outs.push(chunk.toString());
+      });
+      shell.sendCommand({ in: 'foo=5' });
+      await pWaitFor(() => {
+        return outs.length === 1;
+      });
+      let last = Array.from(outs).pop();
+      expect(last).toEqual('5\n');
+      shell.sendCommand({ in: 'foo' });
+      await pWaitFor(() => {
+        return outs.length === 2;
+      });
+      last = Array.from(outs).pop();
+      expect(last).toEqual('5\n');
+    });
+
+    it('can send a big mongo commandcommand', async () => {
       const db = randomDB();
-      mongoShell = new MongoShell('localhost:27017');
       const bigValue =
         'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum';
-      await mongoShell.sendCommand({ in: 'bigValue="' + bigValue + '"' });
-      const resultInsert = await mongoShell.sendCommand({
+      shell = new MongoShell('42', 'localhost:27017', logger);
+      await shell.init();
+      const outs = new Array<string>();
+      shell.stdout.on('data', chunk => {
+        outs.push(chunk.toString());
+      });
+      shell.sendCommand({ in: 'bigValue="' + bigValue + '"' });
+      await pWaitFor(() => {
+        return outs.length === 1;
+      });
+      let last = Array.from(outs).pop();
+      expect(last).toMatch(bigValue);
+      shell.sendCommand({
         in: `db.${db}.insert({value: bigValue})`,
       });
-      expect(resultInsert.out).toEqual('WriteResult({ "nInserted" : 1 })');
-      expect(resultInsert.status).toEqual(SUCCESS);
-      const resultRead = await mongoShell.sendCommand({
-        in: `db.${db}.find({value: bigValue})`,
+
+      await pWaitFor(() => {
+        return outs.length === 2;
       });
-      expect(resultRead.out).toMatch(new RegExp(bigValue));
-      expect(resultInsert.status).toEqual(SUCCESS);
+      last = Array.from(outs).pop();
+      expect(last).toMatch('WriteResult({ "nInserted" : 1 })');
     });
 
     it('can fetch several results up to the iterator end', async () => {
-      const db = randomDB();
       jest.setTimeout(10e3);
-      mongoShell = new MongoShell('localhost:27017');
+      shell = new MongoShell('42', 'localhost:27017', logger);
+      await shell.init();
+      const db = randomDB();
+      const outs = new Array<string>();
+      shell.stdout.on('data', chunk => {
+        outs.push(chunk.toString());
+      });
       for (let index = 0; index < 50; index += 1) {
-        await mongoShell.sendCommand({
+        shell.sendCommand({
           in: `db.${db}.insert({index: ${index}}, { writeConcern: { w: 1, j: true } })`,
         });
       }
-      const resultRead = await mongoShell.sendCommand({
+      await pWaitFor(() => {
+        return outs.length === 50;
+      });
+      shell.sendCommand({
         in: `db.${db}.find({})`,
       });
-      expect(resultRead.out).toMatch(/Type "it" for more/);
-    });
-
-    it('can sends a javascript command to a fresh mongo shell', async () => {
-      mongoShell = new MongoShell('localhost:27017');
-      const result = await mongoShell.sendCommand({ in: 'foo=5' });
-      expect(result.out).toEqual('5');
-      expect(result.status).toEqual(SUCCESS);
+      await pWaitFor(() => {
+        const last = Array.from(outs).pop() || '';
+        return last.includes('for more');
+        // return outs.length === 53;
+      });
+      const last = Array.from(outs).pop();
+      expect(last).toMatch('Type "it" for more');
     });
 
     it('rejects a failed command', async () => {
-      mongoShell = new MongoShell('localhost:27017');
-      const result = await mongoShell.sendCommand({ in: 'idoesnotexist' });
-      expect(result.status).toEqual(ERROR);
-      expect(result.out).toMatch(/idoesnotexist is not defined/);
+      shell = new MongoShell('42', 'localhost:27017', logger);
+      shell.sendCommand({ in: 'idoesnotexist' });
+      const outs = new Array<string>();
+      shell.stdout.on('error', chunk => {
+        outs.push(chunk.toString());
+      });
+      await pWaitFor(() => {
+        return outs.length === 1;
+      });
+      const last = Array.from(outs).pop();
+      expect(last).toMatch('idoesnotexist is not defined');
     });
   });
 });
